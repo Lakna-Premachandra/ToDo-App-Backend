46971db6875a07de600fb84b744c7876c33ba93e
MMNLakna <136817118+MMNLakna@users.noreply.github.com>
2024-05-21 21:08:58 +0530

    Add project files.

---
+++ MyDbContext/MyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDo_App_Backend.MyDbContext
+
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) 
+        {
+            
+        }
+        public DbSet<ToDo> ToDos { get; set; }
+    }
+}
